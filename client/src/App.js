a44184e250c79956729f8f79ed30bdc557777f5f
Max Edmonds <maxedmonds05@gmail.com>
2020-10-06 19:12:18 -0700

    add components, modify css and API.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Nav from "./components/Nav"
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Jumbotron from "./components/Jumbotron";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Jumbotron />
+          <Switch>
+            {/* renders the Search page when "/" route is hit */}
+            <Route exact path="/" component={Search} />
+            {/* renders the Search page when "/search" route is hit */}
+            <Route exact path="/search" component={Search} />
+            {/* renders the Saved page when "/saved" route is hit */}
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
-
 
 export default App;
---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron text-center">
+            <h1>Google Bookworm (A React Application)</h1>
+            <h3><li>Search for books</li></h3>
+            <h3><li>Add them to your reading list by clicking the "Save" button</li></h3>
+
+        </div>
+    );
+}
+
+export default Jumbotron;
